0d3ef246b0faa6445ebf6d1db63bad5ffcf49a6e
daichi <kabata0210@icloud.com>
2025-03-20 21:40:33 +0900

    タスクのデータベースのクラスを作成

--- ToDoList/obj/Debug/net8.0-windows/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net8.0-windows/ToDoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df12065d6e3c81480f44b11e56f762695ca2203d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94e48e639652b57a2508e27c50f6df061a30b54b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
